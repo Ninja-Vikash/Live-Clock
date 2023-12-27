a2f0490ddec01ae737e70fe93b154ff8c77d081a
Vikash Kumar <vikashloharjsr@gmail.com>
2023-12-27 21:45:59 +0530

    themes added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # <a href="https://ninja-vikash.github.io/LIVE-CLOCK/" >🕔 Live Clock</a>
 
-It is a simple digital clock, You can check the exact time, current day, and date with a beautiful background.
+It is a simple digital clock, You can check the exact time, current day, and date with multi themes
 
 
 ### Used components 
--- script.js
+++ script.js
@@ -51,3 +51,43 @@ setInterval(() => {
     currentYear + "-" + currentMonth + "-" + currentDate + " " + currentDay;
   time.innerHTML = currentTime;
 }, 1000);
+
+
+// -------------------------------------- Theme Changer -------------------------------------------
+
+let homeBg = document.getElementById('backgroundImg');
+let homeIcon = document.getElementById('home');
+let fog = document.getElementById('fog')
+let moon = document.getElementById('moon')
+let squirrel = document.getElementById('squirrel')
+let forest = document.getElementById('forest')
+let raccon = document.getElementById('raccon')
+
+homeIcon.addEventListener('click',()=>{
+    console.log('home clicked') 
+    homeBg.style.backgroundImage = 'url(assets/bg-img.avif)'
+    homeIcon.style.outline = '3px solid white'
+    homeIcon.style.transform = 'translateX(0px)'
+})
+
+fog.addEventListener('click',()=>{
+  homeBg.style.backgroundImage = 'url(assets/fog.avif)';
+  homeIcon.style.transform = 'translateX(50px)';
+  homeIcon.style.outline = 'none'
+})
+
+moon.addEventListener('click',()=>{
+  homeBg.style.backgroundImage = 'url(assets/moon.avif)'
+})
+
+forest.addEventListener('click',()=>{
+  homeBg.style.backgroundImage = 'url(assets/forest.avif)'
+})
+
+raccon.addEventListener('click',()=>{
+  homeBg.style.backgroundImage = 'url(assets/raccon.avif)'
+})
+
+squirrel.addEventListener('click',()=>{
+  homeBg.style.backgroundImage = 'url(assets/squirrel.avif)'
+})
