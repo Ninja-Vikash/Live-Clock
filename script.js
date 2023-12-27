f1af764177cf14605c29c497811d8de1a162df02
Vikash Kumar <vikashloharjsr@gmail.com>
2023-12-27 23:17:45 +0530

    code modified

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var myDate;
 var currentDate;
 var currentMonth;
+var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 var currentYear;
 var currentTime;
+var days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
 var dayNumber;
 var date = document.getElementById("date");
 var time = document.getElementById("time");
@@ -13,42 +15,8 @@ setInterval(() => {
   currentYear = myDate.getFullYear();
   currentTime = myDate.toLocaleTimeString();
   dayNumber = myDate.getDay();
-  switch (dayNumber) {
-    case 1: {
-      currentDay = "MON";
-      break;
-    }
-    case 2: {
-      currentDay = "TUE";
-      break;
-    }
-    case 3: {
-      currentDay = "WED";
-      break;
-    }
-    case 4: {
-      currentDay = "THU";
-      break;
-    }
-    case 5: {
-      currentDay = "FRI";
-      break;
-    }
-    case 6: {
-      currentDay = "SAT";
-      break;
-    }
-    case 0: {
-      currentDay = "SUN";
-      break;
-    }
-    default: {
-      currentDate = "ERR";
-      break;
-    }
-  }
   date.innerHTML =
-    currentYear + "-" + currentMonth + "-" + currentDate + " " + currentDay;
+    currentDate + " " + months[currentMonth] + " " + currentYear + " , " + days[dayNumber];
   time.innerHTML = currentTime;
 }, 1000);
 
