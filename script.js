1794451a4749a76ccdb9bf8a10bd11975ef41e87
Vikash Kumar <vikashloharjsr@gmail.com>
2023-12-27 22:53:17 +0530

    theme function updated

--- script.js
+++ script.js
@@ -64,30 +64,61 @@ let forest = document.getElementById('forest')
 let raccon = document.getElementById('raccon')
 
 homeIcon.addEventListener('click',()=>{
-    console.log('home clicked') 
-    homeBg.style.backgroundImage = 'url(assets/bg-img.avif)'
-    homeIcon.style.outline = '3px solid white'
-    homeIcon.style.transform = 'translateX(0px)'
+    homeBg.style.backgroundImage = 'url(assets/bg-img.avif)';
+    homeIcon.style.transform = 'translateX(0px)';
+    fog.style.transform = 'translateX(50px)'
+    moon.style.transform = 'translateX(50px)';
+    forest.style.transform = 'translateX(50px)';
+    raccon.style.transform = 'translateX(50px)';
+    squirrel.style.transform = 'translateX(50px)';
 })
 
 fog.addEventListener('click',()=>{
   homeBg.style.backgroundImage = 'url(assets/fog.avif)';
   homeIcon.style.transform = 'translateX(50px)';
-  homeIcon.style.outline = 'none'
+  fog.style.transform = 'translateX(0px)'
+  moon.style.transform = 'translateX(50px)';
+  forest.style.transform = 'translateX(50px)';
+  raccon.style.transform = 'translateX(50px)';
+  squirrel.style.transform = 'translateX(50px)';
 })
 
 moon.addEventListener('click',()=>{
-  homeBg.style.backgroundImage = 'url(assets/moon.avif)'
+  homeBg.style.backgroundImage = 'url(assets/moon.avif)';
+  homeIcon.style.transform = 'translateX(50px)';
+  fog.style.transform = 'translateX(50px)'
+  moon.style.transform = 'translateX(0px)';
+  forest.style.transform = 'translateX(50px)';
+  raccon.style.transform = 'translateX(50px)';
+  squirrel.style.transform = 'translateX(50px)';
 })
 
 forest.addEventListener('click',()=>{
-  homeBg.style.backgroundImage = 'url(assets/forest.avif)'
+  homeBg.style.backgroundImage = 'url(assets/forest.avif)';
+  homeIcon.style.transform = 'translateX(50px)';
+  fog.style.transform = 'translateX(50px)'
+  moon.style.transform = 'translateX(50px)';
+  forest.style.transform = 'translateX(0px)';
+  raccon.style.transform = 'translateX(50px)';
+  squirrel.style.transform = 'translateX(50px)';
 })
 
 raccon.addEventListener('click',()=>{
-  homeBg.style.backgroundImage = 'url(assets/raccon.avif)'
+  homeBg.style.backgroundImage = 'url(assets/raccon.avif)';
+  homeIcon.style.transform = 'translateX(50px)';
+  fog.style.transform = 'translateX(50px)'
+  moon.style.transform = 'translateX(50px)';
+  forest.style.transform = 'translateX(50px)';
+  raccon.style.transform = 'translateX(0px)';
+  squirrel.style.transform = 'translateX(50px)';
 })
 
 squirrel.addEventListener('click',()=>{
-  homeBg.style.backgroundImage = 'url(assets/squirrel.avif)'
+  homeBg.style.backgroundImage = 'url(assets/squirrel.avif)';
+  homeIcon.style.transform = 'translateX(50px)';
+  fog.style.transform = 'translateX(50px)'
+  moon.style.transform = 'translateX(50px)';
+  forest.style.transform = 'translateX(50px)';
+  raccon.style.transform = 'translateX(50px)';
+  squirrel.style.transform = 'translateX(0px)';
 })
